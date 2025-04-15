893b1545e308965a099eee3c3e4457d33324ee33
AyushInKC <sirfayush1008@gmail.com>
2025-04-15 22:37:39 +0530

    Project Setup

---
+++ src/main/java/com/JavaJunkie/DataHub/DataHubApplication.java
@@ -0,0 +1,13 @@
+package com.JavaJunkie.DataHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JavaJunkie/DataHub/DataHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaJunkie.DataHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
